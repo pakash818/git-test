d859d1bd2192445d154a191ef9add629535b51a5
mrbenjoo <benjamin.sejdic1@gmail.com>
2019-09-12 19:22:34 +0200

    pakash818 modified controller

--- java/client/src/client/Controller.java
+++ java/client/src/client/Controller.java
@@ -8,20 +8,5 @@ import ui.IView;
 import ui.View;
 
 public class Controller implements IController {
-	private IView view;
-	private Client client;
-	private ExecutorService executor;
-	private Socket socket;
-
-	public Controller(ExecutorService executor) {
-		this.executor = executor;
-		this.view = new View(this);
-		view.display();
-	}
-
-
-	public void testFUNC() {
-
-	}
-
+	Pakash818 -> ändrat funktionen;
 }
